e685576b074925f7556107eefa5eff857727a55c
wendellwoney <wendellwoneys@gmail.com>
2018-07-20 12:01:48 -0300

    Bridge Instancia Professor Pos

--- bidge/public/usage.php
+++ bidge/public/usage.php
@@ -3,7 +3,12 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use Bridge\ProfessorGraduacao;
 use Bridge\ProfessorImplGraduacao;
+use Bridge\ProfessorPosGraduacao;
+use Bridge\ProfessorImplPosGraduacao;
 
 $professorGraduacao = new ProfessorGraduacao(new ProfessorImplGraduacao());
 
 print $professorGraduacao->op();
+print "<br>";
+$professorPosGraduacao = new ProfessorPosGraduacao(new ProfessorImplPosGraduacao());
+$professorPosGraduacao->op();
